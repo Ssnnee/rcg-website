29dd0972cda897a5319e9103595d31e0d58dee55
Samuel Nandi <nandisamuelsne@gmail.com>
2024-04-03 15:11:04 +0100

    feat: add a route for database adminstration

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+import Committee from "@/components/Committee/Committee";
+
+export default function AdminPage() {
+  return (
+    <div className="flex min-h-screen justify-center p-24">
+      <Committee />
+    </div>
+  )
+}
