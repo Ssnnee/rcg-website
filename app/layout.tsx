4af785c160ac7bddbf23b3a4b673f6d0a9cc54df
Samuel Nandi <nandisamuelsne@gmail.com>
2024-04-04 05:28:08 +0100

    fiix: put back usefull information for fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "./components/Provider/Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
 
 
 export const metadata: Metadata = {
