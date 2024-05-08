f281c98cfdec4f76bbac2b9e6573a4a18bad1162
Samuel Nandi <nandisamuelsne@gmail.com>
2024-05-08 11:37:40 +0100

    feat: automate the date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Footer () {
   return (
     <footer className="w-full p-4 text-center">
-      <p>© 2024 Revue Congolaise de Gestion - Tout droit réservé</p>
+      <p>© {new Date().getFullYear()} Revue Congolaise de Gestion - Tout droit réservé</p>
     </footer>
   );
 }
