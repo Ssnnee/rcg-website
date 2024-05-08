1399ad50b5f9860e1a67271d8f7de883b857d9e2
Samuel Nandi <nandisamuelsne@gmail.com>
2024-05-08 12:35:08 +0100

    fix: add margin and make the event on the button

--- components/Committee/LectureCommittee.tsx
+++ components/Committee/LectureCommittee.tsx
@@ -8,18 +8,20 @@ import Link from "next/link";
 export default function LectureCommittee() {
   const commiteeMembersList = trpc.member.getMembers.useQuery({committeeId: 2});
   return (
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center'>
+      <div className='grid grid-cols-1 my-5 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center'>
         {commiteeMembersList.data?.map((member, index) => (
           <div key={index} >
             <Card className="sm:w-[500px] md:w-[350px]">
-              <CardTitle className="text-2xl text-center my-2"> {member.name} </CardTitle>
+              <CardTitle className="text-xl text-center my-2"> {member.name} </CardTitle>
               <CardContent className="text-center">
                 <p className="text-lg"> {member.title} </p>
               </CardContent>
               <CardFooter className="">
-                <Button variant={"outline"} className="w-full" >
-                  <Link href={member.resumePdf ?? ''} className="text-blue-500">CV</Link>
-                </Button>
+                <Link
+                  href={member.resumePdf ?? ''}
+                  className="text-blue-500 w-full">
+                  <Button variant={"outline"} className="w-full" >CV</Button>
+                </Link>
               </CardFooter>
             </Card>
           </div>
--- components/Committee/ScientificCommittee.tsx
+++ components/Committee/ScientificCommittee.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { trpc } from "@/server/client";
-import { Separator } from "../ui/separator";
 import { CommitteeSkeleton } from "./CommitteeSkeleton";
-import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "../ui/card";
+import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
@@ -10,18 +9,20 @@ export default function ScientificCommittee() {
   const commiteeMembersList = trpc.member.getMembers.useQuery({committeeId: 1});
 
   return (
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center'>
+      <div className='grid grid-cols-1 my-5 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center'>
         {commiteeMembersList.data?.map((member, index) => (
           <div key={index} >
             <Card className="sm:w-[500px] md:w-[350px]">
-              <CardTitle className="text-2xl text-center my-2"> {member.name} </CardTitle>
+              <CardTitle className="text-xl text-center my-2"> {member.name} </CardTitle>
               <CardContent className="text-center">
                 <p className="text-lg"> {member.title} </p>
               </CardContent>
               <CardFooter className="">
-                <Button variant={"outline"} className="w-full" >
-                  <Link href={member.resumePdf ?? ''} className="text-blue-500">CV</Link>
-                </Button>
+                <Link
+                  href={member.resumePdf ?? ''}
+                  className="text-blue-500 w-full">
+                  <Button variant={"outline"} className="w-full" >CV</Button>
+                </Link>
               </CardFooter>
             </Card>
           </div>
